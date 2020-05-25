66e629c599005b96d94e9de37afc205d81cbbf55
Pablo Settino <pablo.settino@mercadolibre.com>
2020-05-24 21:55:29 -0300

    first commit

---
+++ src/main/java/com/scumbox/mm/commonslibrary/commonslibrary/QueueEvents.java
@@ -0,0 +1,6 @@
+package com.scumbox.mm.commonslibrary.commonslibrary;
+
+public class QueueEvents {
+
+    public static final String SALE_CREATED = "saleCreated";
+}
---
+++ src/main/java/com/scumbox/mm/commonslibrary/commonslibrary/QueueMessage.java
@@ -0,0 +1,26 @@
+package com.scumbox.mm.commonslibrary.commonslibrary;
+
+import java.io.Serializable;
+
+public class QueueMessage implements Serializable {
+
+    private String eventName;
+
+    private Object data;
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
